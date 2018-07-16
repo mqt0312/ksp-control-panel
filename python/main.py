8619164faccaad5d806cd9f44114d84d26702ca3
mqt0312 <mqt0312@users.noreply.github.com>
2018-07-16 14:05:15 -0400

    realize that venv in a repository is quite foolish...

--- python/main.py
+++ python/main.py
@@ -2,36 +2,71 @@ import sys, time, os
 import krpc as ksp
 import serial as ser
 
-
-conn = None
+game = None
+panel = None
 
 def initKSP():
-	print("Initiate connection between client and game")
-	for attempt in range(10):
-		try:
-			conn = ksp.connect()
-			print(conn.krpc.get_status().version)
-			return 1
-		except:
-			time.sleep(2)
-			print('Attempt ' + str(attempt+1) + ' failed')
-	return 0
+    print("Initiate connection between client and game")
+    for attempt in range(10):
+        try:
+            game = ksp.connect()
+            print("Game connected. kRPC version", game.krpc.get_status().version)
+
+            return 1
+        except:
+            time.sleep(1)
+            print('Attempt ' + str(attempt+1) + ' failed')
+            time.sleep(1)
+    return 0
 
 def initArduino():
-	print("Initiate connection between client and controller")
+    print("Initiate connection between client and controller")
+    for attempt in range(10):
 
-def com(data):
+        '''
+        for port in ["COM" + str(num) for num in range(256)]:
+            try:
+                panel = ser.Serial(port, 9600)
+                panel.write('A')
+                if panel.read().decode("utf-8") == "A":
+                    print("Panel connected on port", port)
+                    panel.timeout = None
+                    return 1
+            except:
+                pass
+        '''
+        panel = ser.Serial('COM5', 9600)
+        panel.write(b'A')
+        if panel.read().decode("utf-8") == "A":
+            print("Panel connected on port", port)
+            panel.timeout = None
+            return 1
+        time.sleep(1)
+        print('Attempt ' + str(attempt + 1) + ' failed')
+        time.sleep(1)
+    return 0
+
+
+
+
+def com(data, read_timeout=None, write_timeout=None):
+    panel.timeout = read_timeout
+    panel.write_timeout = write_timeout
+    panel.write(data.encode())
 	
-	return 0
+    return panel.read()
 
 
-def main():
-	print("KSP Control Panel v0.1")
-	print("Copyright (c) 2018 Minh Truong. All right reserved")
-	if ~initKSP():
-		print("Unable to establish connection with the game; perhaps check to see if kRPC server is on?")
+def init():
+    print("KSP Control Panel v0.1")
+    print("Copyright (c) 2018 Minh Truong. All right reserved")
+    if not initKSP():
+        print("Unable to establish connection with the game; perhaps check to see if kRPC server is on?")
+    if not initArduino():
+        print("unable to establish connection with the panel; perhaps check to see if panel is connected to the computer?")
+    #print(com('Hello World'))
 
 print(os.name)
 
 if __name__ == "__main__":
-	main()
+    init()
